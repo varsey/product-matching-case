eeda6aa2a58cc982ca002d6b7be7aee370cb321f
Evgenii Varseev <eugeny.varseev@gmail.com>
2024-06-05 00:27:14 +0300

    docs - add comments to code

--- src/utils.py
+++ src/utils.py
@@ -29,7 +29,7 @@ def get_excluded_list():
     exclude.remove('ya')
     return set(exclude)
 
-
+# Extractor of key words for parent and product groups names extracting
 kw_extractor = yake.KeywordExtractor(
     n=2,
     dedupLim=0.5,
@@ -41,6 +41,10 @@ kw_extractor = yake.KeywordExtractor(
 
 
 def simple_process_item(x: str, exclude: list):
+    '''
+    Lowercase, remove and replace charecters in original string x
+    Remove double spaces after cleaning
+    '''
     x = x.lower()
     for char in remove_chars.keys():
         x = x.replace(char, ' ')
@@ -54,6 +58,7 @@ def simple_process_item(x: str, exclude: list):
 
 
 def count_common_words(str1, str2):
+    '''Method for getting score based on amount of full words intersecting in two strings'''
     set1 = set([x for x in str1.lower().split() if len(x) > 3 and not x.isdigit()])
     set2 = set([x for x in str2.lower().split() if len(x) > 3 and not x.isdigit()])
     if ((len(set1) + len(set2))/2) == 0:
@@ -62,6 +67,7 @@ def count_common_words(str1, str2):
 
 
 def count_common_digits(str1, str2):
+    '''Method for getting score based on amount of numbers intersecting in two strings'''
     set1 = set([x for x in str1.lower().split() if x.isdigit()])
     set2 = set([x for x in str2.lower().split() if x.isdigit()])
     if ((len(set1) + len(set2))/2) == 0:
@@ -70,6 +76,7 @@ def count_common_digits(str1, str2):
 
 
 def count_digit_share(x: str):
+    '''Method for getting share of numbers in the length of string'''
     counter = 0
     for char in x:
         if char.isdigit():
@@ -78,6 +85,10 @@ def count_digit_share(x: str):
 
 
 def remove_numbers(text):
+    '''
+    Method remove all numbers from string and returns 3 first words
+    Words in final string filtered by length - 2 and custom stop words
+    '''
     text = text.lower()
     text = re.sub(r'\d+', '', text)
     text = ' '.join(text.split('.'))
@@ -85,6 +96,7 @@ def remove_numbers(text):
 
 
 def get_product_group(word: str):
+    '''Use YAKE to extract product group'''
     keywords = kw_extractor.extract_keywords(word)
     x = {kw[0]: kw[1] for kw in keywords}
     if len(x) > 0:
@@ -94,6 +106,7 @@ def get_product_group(word: str):
 
 
 def get_parent_group(words: str):
+    '''Use YAKE to extract parent group'''
     if len(words) > 0:
         fltr = kw_extractor.extract_keywords(words)
         fltr_dct = {kw[0]: kw[1] for kw in fltr}
